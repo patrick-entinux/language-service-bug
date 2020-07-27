21d683ee2edee689902102f77ea570e6099450e9
Patrick Miller <patrick.miller@entinux.co.jp>
2020-07-27 16:05:15 +0900

    add ngx-auto-id component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
+import { NgxAutoIdModule } from '@aloreljs/ngx-auto-id';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    NgxAutoIdModule
   ],
   imports: [
     BrowserModule
